47cc912a1f9b410f24e44e0e5a6f8a0eeea08ba1
lucasri-silva <lucasrsilva.dev@gmail.com>
2023-05-11 00:09:19 -0300

    syntax analyzer

--- compiler/Cprogram.c
+++ compiler/Cprogram.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 
 int somar (int a, int b) {
+  int x = 10;
   return a;
 }
 
-int subtrair () {
-  return (a-b);
+int subtrair (int a, int b) {
+  return 10;
 }
 
 int multiplicar (int a, int b) {
-  return (a*b);
-}
+  return (a*b);}
 
 int dividir (int a, int b) {
-  return (a/b);
-}
+  return (a/b);}
 
-int main(void) {
+int main() {
   int a = 10;
   int b = 2;
   scanf("%d",&a);
   scanf("%d",&b);
   int c = somar(a,b);
   int d = subtrair(a,b);
-  int e = multiplicar();
+  int e = multiplicar(a,b);
   int f = dividir(a,b);
   printf("%d\n",a);
   printf("%d\n",d);
   printf("%d\n",e);
   printf("%d\n",f);
-  return 0;
-}
+  return 0;}
--- compiler/syntax_analyzer.py
+++ compiler/syntax_analyzer.py
@@ -2,136 +2,110 @@ import re
 
 errors = 0
 to_match = []
-
-def reposition_file_pointer(file, line):
-    # Get the current position of the file pointer
-    pos = file.tell()
-    # Subtract the length of the current line (including the newline character) to move to the previous line
-    prev_pos = pos - len(line)
-    # Move the file pointer to the new position
-    file.seek(prev_pos)
+# num_lines = 0
 
 def create_new_c_file():
     file = open('Cprogram.c', 'r')
     new_file = open('Cprogram_modified.c', 'w')
+    
+    # num_lines_file = 0
+    # num_lines_newfile = 0
 
     # Read the first line
     line = file.readline()
 
     while line:
+        # num_lines_file += 1
 
         if line.strip():
-            line = file.readline()
-            continue
 
-        if line.strip() == '}':
-            reposition_file_pointer(new_file, line.strip())
-            new_file.write('}\n')
-            line = file.readline()
-            continue
+            if line.strip()[-1] == '{':
+                new_file.write(line.strip()[:-1])
+                new_file.write('\n{\n')
+                line = file.readline()
+                # num_lines_newfile += 2
+                continue
 
-        new_file.write(line.strip()+'\n')
+            if len(line.strip()) > 1 and line.strip()[-1] == '}':
+                new_file.write(line.strip()[:-1])
+                new_file.write('\n}\n')
+                # num_lines_newfile += 2                
+                line = file.readline()
+                continue
+
+            new_file.write(line.strip()+'\n')
+        # num_lines_newfile += 1
         line = file.readline()
-    
+
     file.close()
     new_file.close()
+    # print(num_lines_file)
+    # print(num_lines_newfile)
 
-
-def read_next_line(file):
-    line = file.readline() ## Read the next line
-    return line
 
 def check_regex(line):
 
     global errors
     global to_match
 
-    mainFuncionPattern = r"\w+\s*main\s*\(\s*void\s*\)\s*{?"
+    voidMainFuncionPattern = r"\w+\s*main\s*\(\s*void\s*\)\s*$"
     includePattern = r'^#include\s+((<[^>]+>)|("[^"]+"))$'
     intPattern = r"^(\w+\s+\w+)\s*\=+\s*(\w+)(?:\s*[\+\-\/\*]\s*(\w+))*\;$"
     scanfPattern = r'^scanf\("%([d|ld|f|lf|c|u|lu|x|o])+",&(\w+)\);$'
-    printfPattern = r'^printf\("%([c|d|e|f|g|i|o|p|s|u|x])\\n?",(\w+)\);$'
+    printfPattern = r'^printf\("%([c|d|e|f|g|i|o|p|s|u|x])(\\n)?",(\w+)\);$'
     funcVariableAssignmentPattern = r'^(\w*\s*\w+)+\s*\=+\s*\w+\s*\(\s*(\w+\s*(?:,\s*\w+)*)?\s*\);$'
-    returnFunctionPattern = r'^(\w+\s+\w+)\s*\(\s*((?:\w+\s+\w+\,\s*)*\w+\s+\w+)?\s*\)+\s*{?$'
-
-    print(line)
+    returnFunctionPattern = r'^(\w+\s+\w+)\s*\(\s*((?:\w+\s+\w+\,\s*)*\w+\s+\w+)?\s*\)+\s*$'
 
     ## to match
     if to_match and re.match(to_match[0], line):
-        to_match.pop()
-        print(f'removed ----- {line}')
-        return True
+        to_match.remove(to_match[0])
+        return
 
     ## include
-    if re.match(r'^#?include(.*)$', line):
-        if not re.match(includePattern, line):
-            print(f'ERROR includePattern: {line}')
-            errors += 1
-        
-        print(f'matched {line}')
-        return True
+    if re.match(includePattern, line):
+        return
     
     ## functions that return something
-    elif re.match(r'^(\w+)?\s+(\w+)?\s*\(?\s*((?:(\w+)?\s+\w*,\s*)*((\w+))?\s+(\w*))?\s*\)?\s*\{?$', line):
-        match = re.match(returnFunctionPattern, line)
-        if not match:
-            print(f'ERROR returnFunctionPattern: {line}')
-            errors += 1
-        print(f'matched {line}')
-        regex = r'^return+\s+\w+\;$'
-        to_match.insert(0, regex)
-        # for word in to_match: print(word)
-        print(to_match)
-        return True
+    elif re.match(returnFunctionPattern, line):
+        regex = r'^\s*return\s+.+\s*;$'
+        to_match.append('{')
+        to_match.append(regex)
+        to_match.append('}')
+        return
 
     ## main function
-    elif re.match (r"^int\s+main\s*\(\s*void\s*\)\s*{?$", line):
-        print(f'matched {line}')
-        return True
+    elif re.match (voidMainFuncionPattern, line):
+        if re.match (r"^int\s+main\s*\(\s*void\s*\)\s*$", line):
+            regex = r'^\s*return\s+.+\s*;$'
+            to_match.append('{')
+            to_match.append(regex)
+            to_match.append('}')
+            return
+        to_match.append('{')
+        to_match.append('}')
+        return
     
     ## int a = 10
-    elif re.match(r"^\w+\s+\w+\s*(\=)?\s*\w+(?:\s*[\+\-\/\*]\s*\w+)*\;?$", line):
-        match = re.match(intPattern, line)
-        if not match:
-            print(f'ERROR intPattern: {line}')
-            errors += 1
-        print(f'matched {line}')
-        return True
+    elif re.match(intPattern, line):
+        return
         
     # int a = somar(a, b)   
-    elif re.match(r'^(\w*\s*\w+)?\s*\=+\s*(\w+)?\s*\(\s*(\w+\s*(?:,\s*\w+)*)?\s*\);$', line):
-        match = re.match(funcVariableAssignmentPattern, line)
-        if not match:
-            print(f'ERROR funcVariableAssignmentPattern: {line}')
-            errors += 1
-        print(f'matched {line}')
-        return True
+    elif re.match(funcVariableAssignmentPattern, line):
+        return
         
     ## scanf
-    elif re.match(r'^s+(.*)$', line): 
-        match = re.match(scanfPattern, line)
-        if not match:
-            print(f'ERROR scanfPattern: {line}')
-            errors += 1
-        print(f'matched {line}')
-        return True
+    elif re.match(scanfPattern, line):
+        return
 
     ## prinf
-    elif re.match(r'^p+(.*)$', line):
-        match = re.match(printfPattern, line)
-        if not match:
-            print(f'ERROR printfPattern: {line}')
-            errors += 1
-        print(f'matched {line}')
-        return True
+    elif re.match(printfPattern, line):
+        return
         
     else:
-        print(f'ERROR ultimo: {line}')
+        print(f'ERROR: {line}')
+        return
 
         
-    return False
-
-
 
 def syntax_analysis():
 
@@ -145,10 +119,10 @@ def syntax_analysis():
 
         while line:
 
-            print(line)
             check_regex(line.strip())
-            line = read_next_line(file)
+            line = file.readline()
 
     file.close()
+    print(to_match)
         
     return 1 if errors else 0
